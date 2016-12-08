165e3dc995bd3a4adf9d0466bfc2ed1f053a8b5e
chamto <anrudco@gmail.com>
2016-12-09 00:40:38 +0900

    [AnchorPoint  객체 추가 : Transform 객체의 확장.  AnchorPoint에 따른 크기조정을 한다. ] [AnchorPoint 적용 : 왼손(Left) 큐브에 적용해봄 ]

--- Assets/Script/Sting.cs
+++ Assets/Script/Sting.cs
@@ -2,6 +2,92 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//extend Transform 
+public class AnchorPoint
+{
+	private Vector3 _anchorRate; //[-1~1]
+	private Transform _tr = null;
+
+
+	/// [temp value]
+	Vector3 _movePos;
+	Vector3 _originalPos;
+
+
+	public Vector3 anchorRate 
+	{
+		get
+		{
+			return _anchorRate;
+		}
+
+	}
+	///
+	///this is position value what following anchorRate 
+	/// 
+	public Vector3 position
+	{
+		get
+		{
+			return _originalPos + _movePos;
+		}
+	}
+
+	public AnchorPoint(Transform tr)
+	{
+		_anchorRate = Vector3.zero;
+		_movePos = Vector3.zero;
+
+		_tr = tr;
+		_originalPos = _tr.localPosition;
+	}
+		
+	public void SetPosition(Vector3 pos)
+	{
+		this.calcMovePos ();
+
+		_originalPos = pos - _movePos;
+		_tr.localPosition = pos;
+	}
+
+	public void SetAnchorRate(Vector3 rate)
+	{
+		this.calcMovePos ();
+
+		_anchorRate = rate;
+	}
+
+	private void calcMovePos()
+	{
+
+		//anchor point calc
+		//L = Length
+		//L' = L * Scale
+		//AP = anchor point
+		//MV = move value
+		//(L-L') * AP = MV
+
+		const int CUBE_LENGTH = 1;
+
+		_movePos.x = (CUBE_LENGTH - (CUBE_LENGTH * _tr.localScale.x)) * _anchorRate.x;
+		_movePos.y = (CUBE_LENGTH - (CUBE_LENGTH * _tr.localScale.y)) * _anchorRate.y;
+		_movePos.z = (CUBE_LENGTH - (CUBE_LENGTH * _tr.localScale.z)) * _anchorRate.z;
+	}
+
+	public void Scale(Vector3 scale)
+	{
+		//1
+		_tr.localScale = scale;
+
+		//2
+		this.calcMovePos ();
+
+		//3
+		_tr.localPosition = _originalPos + _movePos;
+
+	}
+}
+
 public class Sting : MonoBehaviour 
 {
 
@@ -9,8 +95,9 @@ public class Sting : MonoBehaviour
 	public Transform _Right	= null;
 	public bool 	_Active = false;
 
-	private Vector3	_leftPos;
-	private Vector3	_leftScale;
+	//private Vector3	_leftPos;
+	//private Vector3	_leftScale;
+	private AnchorPoint _leftAnchor = null;
 
 	private Vector3 _leftGlobalPos;
 
@@ -21,33 +108,27 @@ public class Sting : MonoBehaviour
 	}
 
 
-	bool bOne = false;
+
 	public void AttackLeft()
 	{
-		if (true == bOne)
-			return;
-		bOne = true;
+		//Vector3 pos = _leftPos;
+		//Vector3 scale = _leftScale;
+//		scale.z = 3f;
+//
+//		Vector3 mv = Vector3.zero;
+//		mv.z = (1 - (1 * scale.z)) * -0.5f;
+//		_Left.localPosition = _leftPos + mv;
+//		_Left.localScale = scale;
 
-		Vector3 pos = _leftPos;
-		Vector3 scale = _leftScale;
-
-		//anchor point calc
-		//L = Length
-		//L' = L * Scale
-		//AP = anchor point
-		//MV = move value
-		//(L-L') * AP = MV
-
-
-
+		Vector3 scale = _Left.localScale;
 		scale.z = 3f;
+		_leftAnchor.Scale (scale);
 
-		Vector3 mv = Vector3.zero;
-		mv.z = (1 - (1 * scale.z)) * -0.5f;
-		_Left.localPosition = _leftPos + mv;
-		_Left.localScale = scale;
+
 
 	}
+
+
 	public void AttackRight()
 	{
 		//ref : http://itween.pixelplacement.com/documentation.php
@@ -57,16 +138,19 @@ public class Sting : MonoBehaviour
 	//first stance
 	public void ReadyStance()
 	{
-		_Left.localPosition = _leftPos;
-		_Left.localScale = _leftScale;
+		//_Left.localPosition = _leftPos;
+		//_Left.localScale = _leftScale;
 	}
 
 	// Use this for initialization
 	void Start () 
 	{
 		_leftGlobalPos = _Left.position;
-		_leftPos = _Left.localPosition;
-		_leftScale = _Left.localScale;
+		//_leftPos = _Left.localPosition;
+		//_leftScale = _Left.localScale;
+
+		_leftAnchor = new AnchorPoint (_Left);
+		_leftAnchor.SetAnchorRate (new Vector3 (0,0,-0.5f));
 	}
 	
 	// Update is called once per frame
@@ -75,9 +159,9 @@ public class Sting : MonoBehaviour
 		if (true == _Active) 
 		{
 			this.AttackLeft ();
-			this.AttackLeft_Rotate ();
+			//this.AttackLeft_Rotate ();
 
-			this.AttackRight ();
+			//this.AttackRight ();
 		}
 		_Active = false;
 	}
