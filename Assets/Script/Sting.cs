f149863bae0ce872d4639b02517065d127038891
chamto <anrudco@gmail.com>
2016-12-10 03:46:16 +0900

    [왼손 : 시간이 지남에 따라 일정 거리로 늘어나게 애니메이션 효과 줌 ] [왼손 : 늘어나는 방식 다양화 : iTween의 ease 보간함수 가져다 써봄 : punch , easeOutBounce ]

--- Assets/Script/Sting.cs
+++ Assets/Script/Sting.cs
@@ -27,18 +27,15 @@ public class Sting : MonoBehaviour
 
 	public void AttackLeft()
 	{
-//		Vector3 scale = _Left.localScale;
-//		scale.z = 3f;
-//		_leftAnchor.Scale (scale);
 		_leftAnchor.ScaleZ(3f);
-
 	}
 
 
 	public void AttackRight()
 	{
 		//ref : http://itween.pixelplacement.com/documentation.php
-		iTween.MoveBy(_Right.gameObject, iTween.Hash("z", 2, "easeType", "easeInOutExpo", "loopType", "pingPong", "delay", .1));
+		//iTween.MoveBy(_Right.gameObject, iTween.Hash("z", 2, "easeType", "easeInOutExpo", "loopType", "pingPong", "delay", .1));
+		iTween.PunchPosition(_Right.gameObject, iTween.Hash("z",3,"loopType","loop","time",0.5f));
 	}
 
 	//first stance
@@ -56,16 +53,79 @@ public class Sting : MonoBehaviour
 		//_leftAnchor.SetAnchorRate (new Vector3 (0,0,-0.5f));
 	}
 	
-	// Update is called once per frame
+	float accumulate = 0;
+	float maxSecond = 0.5f;
+	float maxSize = 3f;
 	void Update () 
 	{
+		//min 0s -> 0
+		//    1s -> 1.5
+		//max 2s -> 3
+		//
+		//2 : 3 = dt : ?
+		//3dt = 2? 
+		//3dt/2 = ?
+
+		accumulate += Time.deltaTime;
+		//float scaleDelta = 1 + Mathf.Abs (this.punch (maxSize, accumulate / maxSecond));
+		float scaleDelta = easeOutBounce(1,maxSize,accumulate/maxSecond);
+		//Debug.Log (accumulate + "   " + scaleDelta); //chamto test
+		_leftAnchor.ScaleZ (scaleDelta);
+		//_leftAnchor.ScaleZ(maxSize*accumulate/maxSecond);
+		//Debug.Log (accumulate + "   " + this.punch(maxSize, accumulate/maxSecond)); //chamto test
+		if (maxSecond <= accumulate) {
+			accumulate = 0;
+			//accumulate = maxSecond;
+		}
+
 		if (true == _Active) 
 		{
-			this.AttackLeft ();
+			//this.AttackLeft ();
 			//this.AttackLeft_Rotate ();
 
 			//this.AttackRight ();
 		}
 		_Active = false;
+	}
+
+	private float easeOutQuad(float start, float end, float value){
+		end -= start;
+		return -end * value * (value - 2) + start;
+	}
+
+	private float punch(float amplitude, float percentage){
+		float s = 9;
+		if (percentage == 0){
+			return 0;
+		}
+		if (percentage == 1){
+			return 0;
+		}
+		float period = 1 * 0.3f;
+		s = period / (2 * Mathf.PI) * Mathf.Asin(0);
+		return (amplitude * Mathf.Pow(2, -10 * percentage) * Mathf.Sin((percentage * 1 - s) * (2 * Mathf.PI) / period));
+	}
+
+	private float easeInBounce(float start, float end, float value){
+		end -= start;
+		float d = 1f;
+		return end - easeOutBounce(0, end, d-value) + start;
+	}
+
+	private float easeOutBounce(float start, float end, float value){
+		value /= 1f;
+		end -= start;
+		if (value < (1 / 2.75f)){
+			return end * (7.5625f * value * value) + start;
+		}else if (value < (2 / 2.75f)){
+			value -= (1.5f / 2.75f);
+			return end * (7.5625f * (value) * value + .75f) + start;
+		}else if (value < (2.5 / 2.75)){
+			value -= (2.25f / 2.75f);
+			return end * (7.5625f * (value) * value + .9375f) + start;
+		}else{
+			value -= (2.625f / 2.75f);
+			return end * (7.5625f * (value) * value + .984375f) + start;
+		}
 	}
 }
