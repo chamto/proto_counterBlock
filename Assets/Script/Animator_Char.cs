bd089d5d98449b6246b3f0ba720c2a62aec91fbd
chamto <anrudco@gmail.com>
2016-12-21 02:27:12 +0900

    [ 애니메이터 설정 파일 추가 , 애니메이터 상태값 설정중 ]

---
+++ Assets/Script/Animator_Char.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animator_Char : MonoBehaviour 
+{
+
+	Animator _ani = null;
+
+	void Start () 
+	{
+		_ani = this.GetComponent<Animator> ();	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		//====================================
+		if (Input.GetKeyUp ("up")) 
+		{
+			_ani.SetBool ("attack_up", false);
+		}
+		else if (Input.GetKeyDown ("up")) 
+		{
+			_ani.SetBool ("attack_up", true);
+		} 
+		else if (Input.GetKey ("up")) 
+		{
+			_ani.SetBool ("attack_up", false);
+		}
+			
+
+
+	}
+
+	public void TestAni()
+	{
+		Debug.Log("sdfsdf");
+	}
+}
