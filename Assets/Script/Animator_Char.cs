a4a65f6568c43e6b594e769888fb6aa0039239a3
chamto <anrudco@gmail.com>
2016-12-22 03:24:59 +0900

    [한번만 재생되게 상태머신 수정 ] [애니메이션 재생 테스트용 코드 제작 ] [애니메이션 추가 : 데미지 , 찌르기 ]

--- Assets/Script/Animator_Char.cs
+++ Assets/Script/Animator_Char.cs
@@ -5,6 +5,18 @@ using UnityEngine;
 public class Animator_Char : MonoBehaviour 
 {
 
+	public enum eAniState : int
+	{
+		Idle 			= 0,
+		Attack_up		= 1,
+		Attack_hand		= 2,
+		Attack_middle	= 3,
+		Attack_down		= 4,
+		Attack_sting	= 7,
+		Block_up		= 5,
+		Block_middle	= 6,
+	}
+
 	Animator _ani = null;
 
 	void Start () 
@@ -15,21 +27,48 @@ public class Animator_Char : MonoBehaviour
 	// Update is called once per frame
 	void Update () 
 	{
+
+		//_ani["Attack_up"]
 		//====================================
-		if (Input.GetKeyUp ("up")) 
+		if (Input.GetKeyDown ("up")) 
 		{
-			_ani.SetBool ("attack_up", false);
-		}
-		else if (Input.GetKeyDown ("up")) 
-		{
-			_ani.SetBool ("attack_up", true);
+			_ani.SetInteger ("state", (int)eAniState.Attack_up);
 		} 
 		else if (Input.GetKey ("up")) 
 		{
-			_ani.SetBool ("attack_up", false);
 		}
-			
 
+
+		//====================================
+		if (Input.GetKeyDown ("right")) 
+		{
+			_ani.SetInteger ("state", (int)eAniState.Attack_hand);
+		}
+
+		//====================================
+		if (Input.GetKeyDown ("down")) 
+		{
+			_ani.SetInteger ("state", (int)eAniState.Attack_middle);
+		}
+
+		//====================================
+		if (Input.GetKey ("left") && Input.GetKey("up")) 
+		{
+			_ani.SetInteger ("state", (int)eAniState.Block_up);
+		}
+
+		//====================================
+		if (Input.GetKey ("left") && Input.GetKey("down")) 
+		{
+			_ani.SetInteger ("state", (int)eAniState.Block_middle);
+		}
+
+
+
+		if (Input.GetKeyUp ("up") || Input.GetKeyUp("down") || Input.GetKeyUp("left") || Input.GetKeyUp("right")) 
+		{
+			_ani.SetInteger ("state", (int)eAniState.Idle);
+		}
 
 	}
 
