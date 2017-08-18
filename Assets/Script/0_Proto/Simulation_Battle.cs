4d838f29064b354425bcab5a8dbf13183cf89c86
chamto <anrudco@gmail.com>
2017-08-18 14:22:51 +0900

    [ ___리팩토링___ :  객체 위치 정리  ]

--- Assets/Script/0_Proto/Simulation_Battle.cs
+++ Assets/Script/0_Proto/Simulation_Battle.cs
@@ -52,10 +52,10 @@ namespace CounterBlockSting
 		}
 	}
 
-	public class ComboInfo : List<BehaviorTime>
-	{
-		public Battle.eSkillKind skillKind { get; set; }
-	}
+//	public class ComboInfo : List<BehaviorTime>
+//	{
+//		public Battle.eSkillKind skillKind { get; set; }
+//	}
 
 
 	public class CharacterInfo
@@ -335,8 +335,98 @@ namespace CounterBlockSting
 			}
 
 		}
-	}
 
+
+	}//end class
+
+	public class ResourceMgr 
+	{
+		public enum eSPRITE_NAME
+		{
+			NONE,
+			EMPTY_CARD,
+
+			P1_IDLE,
+			P1_ATTACK_BEFORE,
+			P1_ATTACK_VALID,
+			P1_ATTACK_AFTER,
+			P1_BLOCK_BEFORE,
+			P1_BLOCK_VALID,
+			P1_BLOCK_AFTER,
+
+			P2_IDLE,
+			P2_ATTACK_BEFORE,
+			P2_ATTACK_VALID,
+			P2_ATTACK_AFTER,
+			P2_BLOCK_BEFORE,
+			P2_BLOCK_VALID,
+			P2_BLOCK_AFTER,
+			MAX
+		}
+
+
+
+		private Dictionary<eSPRITE_NAME, string> _sprNameDict = null;
+		private Dictionary<eSPRITE_NAME, Sprite> _loadedDict = new Dictionary<eSPRITE_NAME, Sprite> ();
+
+
+		public void Init()
+		{
+			_sprNameDict = new Dictionary<eSPRITE_NAME, string> ()
+			{
+				{eSPRITE_NAME.EMPTY_CARD, "empty_card"},
+				{eSPRITE_NAME.P1_IDLE, "p1_idle"},
+				{eSPRITE_NAME.P1_ATTACK_BEFORE, "p1_attack_before"},
+				{eSPRITE_NAME.P1_ATTACK_VALID, "p1_attack_valid"},
+				{eSPRITE_NAME.P1_ATTACK_AFTER, "p1_attack_after"},
+				{eSPRITE_NAME.P1_BLOCK_BEFORE, "p1_block_before"},
+				{eSPRITE_NAME.P1_BLOCK_VALID, "p1_block_valid"},
+				{eSPRITE_NAME.P1_BLOCK_AFTER, "p1_block_after"},
+
+				{eSPRITE_NAME.P2_IDLE, "p2_idle"},
+				{eSPRITE_NAME.P2_ATTACK_BEFORE, "p2_attack_before"},
+				{eSPRITE_NAME.P2_ATTACK_VALID, "p2_attack_valid"},
+				{eSPRITE_NAME.P2_ATTACK_AFTER, "p2_attack_after"},
+				{eSPRITE_NAME.P2_BLOCK_BEFORE, "p2_block_before"},
+				{eSPRITE_NAME.P2_BLOCK_VALID, "p2_block_valid"},
+				{eSPRITE_NAME.P2_BLOCK_AFTER, "p2_block_after"}
+			};
+
+		}
+
+		public void Load_BattleCard()
+		{
+			Sprite[] sprites = Resources.LoadAll <Sprite>("Texture/battleCard");
+
+			for(int i=0;i<sprites.Length;i++)
+			{
+				//20170813 chamto fixme - value 값이 없을 때의 예외 처리가 없음 
+				//ref : https://stackoverflow.com/questions/2444033/get-dictionary-key-by-value
+				eSPRITE_NAME key = _sprNameDict.FirstOrDefault(x => x.Value == sprites [i].name).Key;
+				_loadedDict.Add (key, sprites [i]);
+			}
+
+
+		}
+
+		public Sprite GetSprite(eSPRITE_NAME eName)
+		{
+			//20170813 chamto fixme - enum 값이 없을 때의 예외 처리가 없음 
+			return _loadedDict [eName];
+		}
+
+		public void TestPrint()
+		{
+			foreach (Sprite s in _loadedDict.Values) 
+			{
+				DebugWide.LogBlue (s.name);
+			}
+			foreach (eSPRITE_NAME s in _loadedDict.Keys) 
+			{
+				DebugWide.LogBlue (s);
+			}
+		}
+	}//end class
 
 
 	namespace Battle
@@ -504,94 +594,7 @@ namespace CounterBlockSting
 
 		}	
 
-		public class ResourceMgr 
-		{
-			public enum eSPRITE_NAME
-			{
-				NONE,
-				EMPTY_CARD,
 
-				P1_IDLE,
-				P1_ATTACK_BEFORE,
-				P1_ATTACK_VALID,
-				P1_ATTACK_AFTER,
-				P1_BLOCK_BEFORE,
-				P1_BLOCK_VALID,
-				P1_BLOCK_AFTER,
-
-				P2_IDLE,
-				P2_ATTACK_BEFORE,
-				P2_ATTACK_VALID,
-				P2_ATTACK_AFTER,
-				P2_BLOCK_BEFORE,
-				P2_BLOCK_VALID,
-				P2_BLOCK_AFTER,
-				MAX
-			}
-
-
-
-			private Dictionary<eSPRITE_NAME, string> _sprNameDict = null;
-			private Dictionary<eSPRITE_NAME, Sprite> _loadedDict = new Dictionary<eSPRITE_NAME, Sprite> ();
-
-
-			public void Init()
-			{
-				_sprNameDict = new Dictionary<eSPRITE_NAME, string> ()
-				{
-					{eSPRITE_NAME.EMPTY_CARD, "empty_card"},
-					{eSPRITE_NAME.P1_IDLE, "p1_idle"},
-					{eSPRITE_NAME.P1_ATTACK_BEFORE, "p1_attack_before"},
-					{eSPRITE_NAME.P1_ATTACK_VALID, "p1_attack_valid"},
-					{eSPRITE_NAME.P1_ATTACK_AFTER, "p1_attack_after"},
-					{eSPRITE_NAME.P1_BLOCK_BEFORE, "p1_block_before"},
-					{eSPRITE_NAME.P1_BLOCK_VALID, "p1_block_valid"},
-					{eSPRITE_NAME.P1_BLOCK_AFTER, "p1_block_after"},
-
-					{eSPRITE_NAME.P2_IDLE, "p2_idle"},
-					{eSPRITE_NAME.P2_ATTACK_BEFORE, "p2_attack_before"},
-					{eSPRITE_NAME.P2_ATTACK_VALID, "p2_attack_valid"},
-					{eSPRITE_NAME.P2_ATTACK_AFTER, "p2_attack_after"},
-					{eSPRITE_NAME.P2_BLOCK_BEFORE, "p2_block_before"},
-					{eSPRITE_NAME.P2_BLOCK_VALID, "p2_block_valid"},
-					{eSPRITE_NAME.P2_BLOCK_AFTER, "p2_block_after"}
-				};
-
-			}
-
-			public void Load_BattleCard()
-			{
-				Sprite[] sprites = Resources.LoadAll <Sprite>("Texture/battleCard");
-
-				for(int i=0;i<sprites.Length;i++)
-				{
-					//20170813 chamto fixme - value 값이 없을 때의 예외 처리가 없음 
-					//ref : https://stackoverflow.com/questions/2444033/get-dictionary-key-by-value
-					eSPRITE_NAME key = _sprNameDict.FirstOrDefault(x => x.Value == sprites [i].name).Key;
-					_loadedDict.Add (key, sprites [i]);
-				}
-
-
-			}
-
-			public Sprite GetSprite(eSPRITE_NAME eName)
-			{
-				//20170813 chamto fixme - enum 값이 없을 때의 예외 처리가 없음 
-				return _loadedDict [eName];
-			}
-
-			public void TestPrint()
-			{
-				foreach (Sprite s in _loadedDict.Values) 
-				{
-					DebugWide.LogBlue (s.name);
-				}
-				foreach (eSPRITE_NAME s in _loadedDict.Keys) 
-				{
-					DebugWide.LogBlue (s);
-				}
-			}
-		}
 
 		public enum eSkillKind
 		{
@@ -606,6 +609,11 @@ namespace CounterBlockSting
 
 			CounterBlock,
 			Max
+		}
+
+		public class ComboInfo : List<BehaviorTime>
+		{
+			public Battle.eSkillKind skillKind { get; set; }
 		}
 
 		public class SkillMgr
