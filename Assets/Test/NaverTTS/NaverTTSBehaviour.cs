7891c1d8f1526d3e9dc1c9699f93d874ae41c7b7
chamto <anrudco@gmail.com>
2017-12-20 15:34:58 +0900

    [ NaverTTS : 언어,성별,속도 설정할 수 있게 기능 추가 ]

--- Assets/Test/NaverTTS/NaverTTSBehaviour.cs
+++ Assets/Test/NaverTTS/NaverTTSBehaviour.cs
@@ -1,22 +1,106 @@
-﻿using System.Collections;
+﻿using System;
+using System.Net;
+using System.Text;
+using System.IO;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 
-// 네이버 음성합성 Open API 예제
-using System;
-using System.Net;
-using System.Text;
-using System.IO;
-
-//naver TTS 계정 
-//https://developers.naver.com/apps/#/myapps/2Lpz1sFLrJhFy1tY9SFR/overview
-
 //아마존 폴리 TTS link
 //https://us-east-2.console.aws.amazon.com/polly/home/SynthesizeSpeech
-public static class NaverTTS
+
+//naver TTS 
+//계정 : https://developers.naver.com/apps/#/myapps/2Lpz1sFLrJhFy1tY9SFR/overview
+//문서 : https://developers.naver.com/docs/clova/api/#/CSS/API_Guide.md#RequestParameter
+public class NaverTTS
 {
-	public static void Response(string text)
+	//-5이면 0.5배 빠른 속도이고 5이면 0.5배 느린 속도입니다
+	public const int MAX_SPEED = -5;
+	public const int MIN_SPEED = 5;
+	public const int BASIC_SPEED = 0;
+
+	public enum eLanguage
+	{
+		Korean,
+		English,
+		Chinese,
+		Spanish,
+		Japanese,
+	}
+
+	public enum eSex
+	{
+		Man,
+		Woman,
+	}
+
+	private int 	_speed = BASIC_SPEED;
+	private string 	_speaker = "jinho";
+
+	public void SetSpeaker(eLanguage lang , eSex sex)
+	{
+		//	mijin : 한국어, 여성 음색
+		//	jinho : 한국어, 남성 음색
+		//	clara : 영어, 여성 음색
+		//	matt : 영어, 남성 음색
+		//	yuri : 일본어, 여성 음색
+		//	shinji : 일본어, 남성 음색
+		//	meimei : 중국어, 여성 음색
+		//	liangliang : 중국어, 남성 음색
+		//	jose : 스페인어, 남성 음색
+		//	carmen : 스페인어, 여성 음색
+
+		string temp = "";
+		switch (lang) 
+		{
+		case eLanguage.Korean:
+			if (sex == eSex.Man)
+				temp = "jinho";
+			if (sex == eSex.Woman)
+				temp = "mijin";
+			break;
+		case eLanguage.English:
+			if (sex == eSex.Man)
+				temp = "matt";
+			if (sex == eSex.Woman)
+				temp = "clara";
+			break;
+		case eLanguage.Chinese:
+			if (sex == eSex.Man)
+				temp = "liangliang";
+			if (sex == eSex.Woman)
+				temp = "meimei";
+			break;
+		case eLanguage.Spanish:
+			if (sex == eSex.Man)
+				temp = "jose";
+			if (sex == eSex.Woman)
+				temp = "carmen";
+			break;
+		case eLanguage.Japanese:
+			if (sex == eSex.Man)
+				temp = "shinji";
+			if (sex == eSex.Woman)
+				temp = "yuri";
+			break;
+		}
+
+		_speaker = temp;
+	}
+
+	public void SetSpeed(int speed)
+	{
+		if (MAX_SPEED < speed)
+			speed = MAX_SPEED;
+
+		if (MIN_SPEED > speed)
+			speed = MIN_SPEED;
+
+		_speed = speed;
+	}
+
+	public void Request(string text)
 	{
 		//ServicePointManager.ServerCertificateValidationCallback = MyRemoteCertificateValidationCallback;
 		//System.Diagnostics.Process.Start("mozroots","--import --quiet");
@@ -28,7 +112,7 @@ public static class NaverTTS
 		request.Headers.Add("X-Naver-Client-Secret", "epWAWPAWNX");
 		request.Method = "POST";
 
-		byte[] byteDataParams = Encoding.UTF8.GetBytes("speaker=mijin&speed=0&text=" + text);
+		byte[] byteDataParams = Encoding.UTF8.GetBytes("speaker="+_speaker+"&speed="+_speed+"&text=" + text);
 		request.ContentType = "application/x-www-form-urlencoded";
 		request.ContentLength = byteDataParams.Length;
 		Stream st = request.GetRequestStream();
@@ -40,13 +124,15 @@ public static class NaverTTS
 		DebugWide.LogBlue ("status="+ status);
 		//Console.WriteLine("status="+ status);
 
+		File.Delete ("Assets/StreamingAssets/" + "tts.mp3");
+		
 		using (Stream output = File.OpenWrite ("Assets/StreamingAssets/" + "tts.mp3")) 
 		{
 			using (Stream input = response.GetResponseStream ()) 
 			{
 			
 				//input.CopyTo(output);
-				NaverTTS.CopyTo(input, output);
+				this.CopyTo(input, output);
 
 			}
 		}
@@ -56,7 +142,7 @@ public static class NaverTTS
 	}
 
 	//ref : https://stackoverflow.com/questions/5730863/how-to-use-stream-copyto-on-net-framework-3-5
-	public static void CopyTo(this Stream input, Stream output)
+	public void CopyTo(Stream input, Stream output)
 	{
 		byte[] buffer = new byte[16 * 1024]; // Fairly arbitrary size
 		int bytesRead;
@@ -73,11 +159,14 @@ public static class NaverTTS
 
 public class NaverTTSBehaviour : MonoBehaviour 
 {
-
+	NaverTTS _tts = new NaverTTS();
 	// Use this for initialization
 	void Start () 
 	{
-		NaverTTS.Response ("안녕 하시렵니까  드루와 드루와 ㄲㄲㄲㄱ 드루와 드루와 ㄲㄲㄲㄱ드루와 드루와 ㄲㄲㄲㄱ드루와 ");
+		_tts.SetSpeaker (NaverTTS.eLanguage.Korean, NaverTTS.eSex.Man);
+		_tts.SetSpeed (0);
+		//_tts.Request ("안녕 하시렵니까  드루와 드루와 ㄲㄲㄲㄱ 드루와 드루와 ㄲㄲㄲㄱ드루와 드루와 ㄲㄲㄲㄱ드루와 ");
+		_tts.Request ("Update is called once per frame 안녕");
 	}
 	
 	// Update is called once per frame
