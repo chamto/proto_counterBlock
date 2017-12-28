388295005c6a9d464a45fe07608dfab56188b644
chamto <anrudco@gmail.com>
2017-12-29 08:36:10 +0900

    [ UI씬 제어 객체 추가 : UI_Game , UI_Loading ]

---
+++ Assets/Script/UI_Game.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_Game : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+		CanvasRenderer renderer = GameObject.Find ("Panel_root").GetComponent<CanvasRenderer>();
+
+		StartCoroutine (GlobalFunctions.FadeIn (renderer , 1.0f));		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
