cbb8502c22582ba3f9c9dd3cc4593e6e013e630d
chamto <anrudco@gmail.com>
2018-07-07 03:39:33 +0900

    [ <PullOut 프로젝트> : 키입력 처리에서 이동처리 분리 (AI에서 사용하기 위한 밑작업) ]

--- Assets/z_ProtoGame/PullOut/PullOut.cs
+++ Assets/z_ProtoGame/PullOut/PullOut.cs
@@ -7,11 +7,13 @@ using Utility;
 public class PullOut : MonoBehaviour 
 {
 
-    public Transform _knight = null;
+    public Transform _target = null;
 
 	// Use this for initialization
-	void Start () {
-		
+	void Start () 
+    {
+        ProtoGame.KeyInput keyInput = gameObject.AddComponent<ProtoGame.KeyInput>();
+        keyInput.SetMoveTarget(_target);
 	}
 	
 	// Update is called once per frame
@@ -20,84 +22,167 @@ public class PullOut : MonoBehaviour
 	}
 
 
-    void FixedUpdate()
-    {
-        this.Up();
-        this.Down();
-        this.Left();
-        this.Right();
-    }
 
-
-    float accumulate_up = 0;
-    public void Up()
-    {
-        if (Input.GetKeyUp("up"))
-        {
-            accumulate_up = 0;
-            //Debug.Log ("key-up: up state");
-        }
-        else if (Input.GetKey("up"))
-        {
-            const float MAX_SECOND = 0.5f;
-            if (accumulate_up > MAX_SECOND)
-                accumulate_up = 0;
-
-            accumulate_up += Time.deltaTime;
-            float delta = Interpolation.easeInOutBack(0f, 0.2f, accumulate_up / MAX_SECOND);
-            _knight.Translate(Vector3.forward * delta);
-            //Debug.Log ("key-up: down state");
-
-        }
-    }
-
-    public void Down()
-    {
-        if (Input.GetKey("down"))
-        {
-            _knight.Translate(Vector3.back * Time.deltaTime * 4f); //one second per 4 move
-                                                                   //Debug.Log ("down");
-        }
-    }
-
-    float accumulate_left = 0;
-    public void Left()
-    {
-        if (Input.GetKeyUp("left"))
-        {
-            accumulate_left = 0;
-        }
-        else if (Input.GetKey("left"))
-        {
-            const float MAX_SECOND = 0.2f;
-            if (accumulate_left > MAX_SECOND)
-                accumulate_left = 0;
-
-            accumulate_left += Time.deltaTime;
-            //float delta = Interpolation.easeInOutBack(0f, 5f, accumulate_left / MAX_SECOND);
-            float delta = Interpolation.easeOutCirc(0f, 5f, accumulate_left / MAX_SECOND);
-            _knight.Rotate(Vector3.up, -1 * delta);
-        }
-    }
-
-    float accumulate_right = 0;
-    public void Right()
-    {
-        if (Input.GetKeyUp("right"))
-        {
-            accumulate_right = 0;
-        }
-        else if (Input.GetKey("right"))
-        {
-            const float MAX_SECOND = 0.2f;
-            if (accumulate_right > MAX_SECOND)
-                accumulate_right = 0;
-
-            accumulate_right += Time.deltaTime;
-            //float delta = Interpolation.easeInOutBack(0f, 5f, accumulate_right / MAX_SECOND);
-            float delta = Interpolation.easeOutCirc(0f, 5f, accumulate_right / MAX_SECOND);
-            _knight.Rotate(Vector3.up, 1 * delta);
-            //Debug.Log ("r");
-        }
-    }
 }
+
+
+namespace ProtoGame
+{
+    public class CallInterval
+    {
+        private float _accumulate = 0f;
+        private float _prev_accumulate = 0f;
+        private float _CALL_TIME_INTERVAL = 0.2f; //함수 호출 간격이 0.2초 이하면 연속호출 상태라 가정함
+
+
+        public float GetTimeAccumulate(float MAX_SECOND)
+        {
+            if ((_accumulate - _prev_accumulate) < _CALL_TIME_INTERVAL)
+            {   //**** 연속호출 상태 *****
+
+                _accumulate += Time.deltaTime;
+
+                if (_accumulate > MAX_SECOND)
+                    _accumulate = 0;
+
+            }
+            else
+            {   //**** 불연속호출 상태 ****
+                _accumulate = 0;
+            }
+
+            _prev_accumulate = _accumulate;
+
+            return _accumulate;
+        }
+    }
+
+    public class Move
+    {
+        
+        public Transform _target = null;
+
+        private CallInterval _Interval_Up = new CallInterval();
+        private CallInterval _Interval_Left = new CallInterval();
+        private CallInterval _Interval_Right = new CallInterval();
+
+
+        public void Up(float MAX_SECOND)
+        {
+            MAX_SECOND = 0.5f; //test value
+
+            //연속호출 상황시 시간증가값 구하기
+            float accumulate = _Interval_Up.GetTimeAccumulate(MAX_SECOND);
+
+
+            //보간, 이동 처리
+            float delta = Interpolation.easeInOutBack(0f, 0.2f, accumulate / MAX_SECOND);
+            _target.Translate(Vector3.forward * delta);
+
+        }
+
+        public void Down(float MAX_SECOND)
+        {
+            MAX_SECOND = 4f; //test value
+
+            _target.Translate(Vector3.back * Time.deltaTime * MAX_SECOND); //one second per 4 move
+
+        }
+
+
+        public void Left(float MAX_SECOND)
+        {
+            MAX_SECOND = 0.2f; //test value
+
+            //연속호출 상황시 시간증가값 구하기
+            float accumulate = _Interval_Up.GetTimeAccumulate(MAX_SECOND);
+
+
+            //보간, 이동 처리
+            float delta = Interpolation.easeOutCirc(0f, 5f, accumulate / MAX_SECOND);
+            _target.Rotate(Vector3.up, -1 * delta);
+        }
+
+
+        public void Right(float MAX_SECOND)
+        {
+            MAX_SECOND = 0.2f; //test value
+
+            //연속호출 상황시 시간증가값 구하기
+            float accumulate = _Interval_Up.GetTimeAccumulate(MAX_SECOND);
+
+
+            //보간, 이동 처리
+            float delta = Interpolation.easeOutCirc(0f, 5f, accumulate / MAX_SECOND);
+            _target.Rotate(Vector3.up, 1 * delta);
+        }
+    }
+
+    public class KeyInput : MonoBehaviour 
+    {
+        private Move _move = new Move();
+
+        public void SetMoveTarget(Transform target)
+        {
+            _move._target = target;
+        }
+		
+
+		void FixedUpdate()
+        {
+            this.Up();
+            this.Down();
+            this.Left();
+            this.Right();
+        }
+
+
+        public void Up()
+        {
+            if (Input.GetKeyUp("up"))
+            {
+                //Debug.Log ("key-up: up state");
+            }
+            else if (Input.GetKey("up"))
+            {
+                //Debug.Log ("key-up: down state");
+                _move.Up(0f);
+            }
+        }
+
+        public void Down()
+        {
+            if (Input.GetKey("down"))
+            {
+                _move.Down(0f);   
+            }
+        }
+
+        
+        public void Left()
+        {
+            if (Input.GetKeyUp("left"))
+            {
+                
+            }
+            else if (Input.GetKey("left"))
+            {
+                _move.Left(0f);
+            }
+        }
+
+        
+        public void Right()
+        {
+            if (Input.GetKeyUp("right"))
+            {
+                
+            }
+            else if (Input.GetKey("right"))
+            {
+                _move.Right(0f);
+            }
+        }
+    }//end class
+
+}//end namespace
