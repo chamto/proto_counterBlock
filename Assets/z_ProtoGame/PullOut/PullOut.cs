2dc33e968d74101ca29e8585a436848da3144caa
chamto <anrudco@gmail.com>
2018-07-13 02:35:15 +0900

    [ <PullOut 쳐내기 프로젝트> ] [ 게임 바닥풀장 크기 5배 키움 : 강체에 y축 프리즈를 걸어도 풀장을 벗어나면 제어가 안되는 문제 때문에 풀장크기를 키운것임 ] [ 인공지능 모드 활성화시 상대 캐릭터 따라가도록 작업함 ]

--- Assets/z_ProtoGame/PullOut/PullOut.cs
+++ Assets/z_ProtoGame/PullOut/PullOut.cs
@@ -28,11 +28,11 @@ public class PullOut : MonoBehaviour
         //========================================================================
 
         ProtoGame.KeyInput key1p = _target_1p.gameObject.AddComponent<ProtoGame.KeyInput>();
-        key1p.SetTarget(_target_1p);
+        key1p.SetMainBody(_target_1p);
         key1p.SelectPlayerNum(ProtoGame.KeyInput.ePlayerNum.Player_1);
 
         _ai_1p = _target_1p.gameObject.AddComponent<ProtoGame.AI>();
-        _ai_1p.SetTarget(_target_1p);
+        _ai_1p.SetMainBody(_target_1p);
         _ai_1p.enabled = false;
 
         _ai_1p._ref_objects = _objects;
@@ -41,11 +41,11 @@ public class PullOut : MonoBehaviour
 
 
         ProtoGame.KeyInput key2p = _target_2p.gameObject.AddComponent<ProtoGame.KeyInput>();
-        key2p.SetTarget(_target_2p);
+        key2p.SetMainBody(_target_2p);
         key2p.SelectPlayerNum(ProtoGame.KeyInput.ePlayerNum.Player_2);
 
         _ai_2p = _target_2p.gameObject.AddComponent<ProtoGame.AI>();
-        _ai_2p.SetTarget(_target_2p);
+        _ai_2p.SetMainBody(_target_2p);
         _ai_2p.enabled = false;
 
         _ai_2p._ref_objects = _objects;
@@ -86,6 +86,23 @@ namespace ProtoGame
     public class GObjects
     {
         public List<Transform> _characters = new List<Transform>();
+
+
+        //최대 반경이내에서 가장 가까운 객체를 반환한다
+        public Transform GetNearCharacter(Transform exceptChar , float maxRadius)
+        {
+
+            //todo : 추후구현하기
+
+            foreach(Transform t in _characters)
+            {
+                if (t == exceptChar) continue;
+
+                return t;
+            }
+
+            return null;
+        }
     }
 
 }//end namespace 
@@ -98,6 +115,7 @@ namespace ProtoGame
 {
     public class AI : MonoBehaviour
     {
+        private Transform _mainBody = null;
         private Transform _target = null;
         private Move _move = new Move();
 
@@ -113,15 +131,15 @@ namespace ProtoGame
         }
         private eState _state = eState.Roaming;
 
-        public void SetTarget(Transform target)
+        public void SetMainBody(Transform mainBody)
         {
-            _target = target;
-            _move._target = target;
+            _mainBody = mainBody;
+            _move._mainBody = mainBody;
         }
 
 		private void Update()
 		{
-            if (null == _target) return;
+            if (null == _mainBody) return;
 
             this.StateUpdate();
 		}
@@ -205,7 +223,27 @@ namespace ProtoGame
                             _state = eState.Detect;
                         }
 
-                        //_move.Up(0f); //test
+
+
+                        _target = _ref_objects.GetNearCharacter(_mainBody, 10f); 
+
+                        switch(_move.DirectionalInspection(_target.localPosition))
+                        {
+                            case Move.eDirection.LEFT:
+                                {
+                                    _move.Left(0f);
+                                    //DebugWide.LogBlue("LEFT");
+                                }
+                                break;
+                            case Move.eDirection.RIGHT:
+                                {
+                                    _move.Right(0f);
+                                    //DebugWide.LogBlue("RIGHT");
+                                }
+                                break;
+                        }
+
+                        _move.Up(0f);
 
                 }
                 break;
@@ -255,8 +293,14 @@ namespace ProtoGame
 
     public class Move
     {
-        
-        public Transform _target = null;
+        public enum eDirection
+        {
+            CENTER,
+            LEFT,
+            RIGHT,
+        }
+
+        public Transform _mainBody = null;
 
         private CallInterval _Interval_Up = new CallInterval();
         private CallInterval _Interval_Left = new CallInterval();
@@ -267,13 +311,15 @@ namespace ProtoGame
         {
             MAX_SECOND = 0.5f; //test value
 
+            //0.2거리 가는데 0.5초 걸림
+
             //연속호출 상황시 시간증가값 구하기
             float accumulate = _Interval_Up.GetTimeAccumulate(MAX_SECOND);
 
 
             //보간, 이동 처리
             float delta = Interpolation.easeInOutBack(0f, 0.2f, accumulate / MAX_SECOND);
-            _target.Translate(Vector3.forward * delta);
+            _mainBody.Translate(Vector3.forward * delta);
 
         }
 
@@ -281,7 +327,7 @@ namespace ProtoGame
         {
             MAX_SECOND = 4f; //test value
 
-            _target.Translate(Vector3.back * Time.deltaTime * MAX_SECOND); //one second per 4 move
+            _mainBody.Translate(Vector3.back * Time.deltaTime * MAX_SECOND); //one second per 4 move
 
         }
 
@@ -290,13 +336,15 @@ namespace ProtoGame
         {
             MAX_SECOND = 0.2f; //test value
 
+            float angle = 5f; //5도 회전하는데 0.2초 걸림 
+
             //연속호출 상황시 시간증가값 구하기
             float accumulate = _Interval_Up.GetTimeAccumulate(MAX_SECOND);
 
 
             //보간, 이동 처리
-            float delta = Interpolation.easeOutCirc(0f, 5f, accumulate / MAX_SECOND);
-            _target.Rotate(Vector3.up, -1 * delta);
+            float delta = Interpolation.easeOutCirc(0f, angle, accumulate / MAX_SECOND);
+            _mainBody.Rotate(Vector3.up, -1 * delta);
         }
 
 
@@ -304,13 +352,52 @@ namespace ProtoGame
         {
             MAX_SECOND = 0.2f; //test value
 
+            float angle = 5f;
+
             //연속호출 상황시 시간증가값 구하기
             float accumulate = _Interval_Up.GetTimeAccumulate(MAX_SECOND);
 
 
             //보간, 이동 처리
-            float delta = Interpolation.easeOutCirc(0f, 5f, accumulate / MAX_SECOND);
-            _target.Rotate(Vector3.up, 1 * delta);
+            float delta = Interpolation.easeOutCirc(0f, angle, accumulate / MAX_SECOND);
+            _mainBody.Rotate(Vector3.up, 1 * delta);
+        }
+
+
+        //객체의 전진 방향을 반환한다.
+        public Vector3 GetForwardDirect()
+        {
+            return Quaternion.Euler(_mainBody.localEulerAngles) * Vector3.forward;
+        }
+
+
+        //내방향을 기준으로 목표위치가 어디쪽에 있는지 반환한다.  
+        public eDirection DirectionalInspection(Vector3 targetPos)
+        {
+
+            Vector3 mainDir = GetForwardDirect();
+
+            Vector3 targetTo = targetPos - _mainBody.localPosition;
+
+            //mainDir.Normalize();
+            //targetTo.Normalize();
+
+            Vector3 dir = Vector3.Cross(mainDir, targetTo);
+            //dir.Normalize();
+            //DebugWide.LogBlue("mainDir:" + mainDir + "  targetTo:" + targetTo + "   cross:" + dir.y);
+
+            float angle = Vector3.Angle(mainDir, targetTo);
+            angle = Mathf.Abs(angle);
+
+            if (angle < 3f) return eDirection.CENTER; //사이각이 3도 보다 작다면 중앙으로 여긴다 
+
+            //외적의 y축값이 음수는 왼쪽방향 , 양수는 오른쪽방향 
+            if (dir.y < 0)
+                return eDirection.LEFT;
+            else if (dir.y > 0)
+                return eDirection.RIGHT;
+
+            return eDirection.CENTER; 
         }
 
     }
@@ -344,9 +431,9 @@ namespace ProtoGame
             _playerNum = playerNum;
         }
 
-        public void SetTarget(Transform target)
+        public void SetMainBody(Transform mainBody)
         {
-            _move._target = target;
+            _move._mainBody = mainBody;
         }
 		
 
