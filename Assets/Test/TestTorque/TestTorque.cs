976bd5a2a0240bca50f072e6a426613a3d05cb4a
chamto <anrudco@gmail.com>
2018-07-14 02:11:23 +0900

    [ 물리 토크 테스트용 프로젝트 추가 ]

---
+++ Assets/Test/TestTorque/TestTorque.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestTorque : MonoBehaviour 
+{
+
+    private Rigidbody _rigid = null;
+
+    //ref : https://docs.unity3d.com/kr/530/ScriptReference/ForceMode.html
+    public ForceMode _fMode = ForceMode.Acceleration;
+
+	// Use this for initialization
+	void Start () 
+    {
+        _rigid = this.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    public float amount = 50f;
+
+
+    void FixedUpdate()
+    {
+        float h = Input.GetAxis("Horizontal") * amount * Time.deltaTime;
+        float v = Input.GetAxis("Vertical") * amount * Time.deltaTime;
+
+        _rigid.AddTorque(transform.up * h, _fMode);
+        _rigid.AddTorque(transform.right * v, _fMode);
+
+        //_rigid.AddRelativeTorque(transform.up * h, _fMode);
+        //_rigid.AddRelativeTorque(transform.right * v, _fMode);
+    }
+}
